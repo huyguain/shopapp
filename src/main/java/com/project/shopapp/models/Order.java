01bf36136b7b000337146a14b2bb8b7f31685548
huy.nguyen21 <huy.nguyen21@onemount.com>
2024-10-21 11:15:28 +0700

    2.17. models Order, OrderDetail

---
+++ src/main/java/com/project/shopapp/models/Order.java
@@ -0,0 +1,72 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "note", length = 100)
+    private String note;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_money")
+    private Integer totalMoney;
+
+    @Column(name = "shipping_method")
+    private String shippingMethod;
+
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+
+    @Column(name = "shipping_date")
+    private LocalDateTime shippingDate;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "payment_date")
+    private Date paymentDate;
+
+    @Column(name = "active")
+    private boolean active;
+}
