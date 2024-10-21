bb8846205c1f2253a231b900fd10867fa7f08833
huy.nguyen21 <huy.nguyen21@onemount.com>
2024-10-21 11:14:50 +0700

    2.16. models Role, User, ProductIamge

---
+++ src/main/java/com/project/shopapp/models/User.java
@@ -0,0 +1,50 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fullName", length = 100)
+    private String fullName;
+
+    @Column(name = "phone_number", length = 10, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false, length = 255)
+    private String address;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "facebook_account_id")
+    private int facebookAccountId;
+
+    @Column(name = "google_account_id")
+    private int googleAccountId;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
